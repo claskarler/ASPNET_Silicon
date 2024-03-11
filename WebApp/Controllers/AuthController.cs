c8e590324e4feb92fe6f8ef50d6003169346535f
Clara <cskarler@gmail.com>
2024-03-11 18:11:34 +0100

    Sign up is completed

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
 
 public class AuthController : Controller
 {
     [Route("/signup")]
+    [HttpGet]
     public IActionResult SignUp()
     {
-        return View();
+        var viewModel = new SignUpViewModel();
+        return View(viewModel);
+    }
+
+    
+    [Route("/signup")]
+    [HttpPost]
+    public IActionResult SignUp(SignUpViewModel viewModel)
+    {
+        if (!ModelState.IsValid) 
+        {
+            return View(viewModel);
+        }
+        return RedirectToAction("SignIn", "Auth");
     }
 }
