79f8b9ed2b1601bd0db553780a5e8b24cf7ef9af
Clara <cskarler@gmail.com>
2024-03-14 02:45:45 +0100

    account details completed

---
+++ WebApp/ViewModels/AccountDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels;
+
+public class AccountDetailsViewModel
+{
+    public string Title { get; set; } = "Account Details";
+    public AccountDetailsBasicInfoModel BasicInfo { get; set; } = new AccountDetailsBasicInfoModel()
+    {
+        ProfileImage = "images/profile-image.svg",
+        FirstName = "Bonnie",
+        LastName = "Hunden",
+        Email = "bonnie@hunden.com"
+    };
+    public AccountDetailsAddressInfoModel AddressInfo { get; set; } = new AccountDetailsAddressInfoModel();
+}
